a5a4fac2c18753e97fdee440cb0d46658a958a45
Musab Alradi <78719015+musabalradi-dev@users.noreply.github.com>
2025-04-20 10:42:52 +0200

    first commit

--- lib/feature/home/presentation/screen/home.dart
+++ lib/feature/home/presentation/screen/home.dart
@@ -9,77 +9,85 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Home')),
       body: SizedBox(
         width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // صورة الخلفية مع تأثير Blur
-            AnimatedBuilder(
-              animation: PageController(),
-              builder: (context, child) {
-                return Transform.scale(
-                  scale: 1.0,
-                  child: Image.asset(
-                    'assets/images/welcome.png',
-                    fit: BoxFit.cover,
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    height: MediaQuery.of(context).size.height * 0.4,
-                    filterQuality: FilterQuality.high,
-                  ),
-                );
-              },
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Welcome to the application',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Show a dialog or navigate to the details screen
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Congratulations', textAlign: TextAlign.center),
-                      content: Text(
-                        'You have successfully completed the onboarding process.',
-                        textAlign: TextAlign.center,
-                      ),
-                      titlePadding: EdgeInsets.all(20),
-                      contentTextStyle: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                      titleTextStyle: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      backgroundColor: Colors.white,
-                      contentPadding: EdgeInsets.all(20),
-                      actionsPadding: EdgeInsets.all(10),
-                      actionsAlignment: MainAxisAlignment.center,
-                      actionsOverflowButtonSpacing: 10,
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text('Close', textAlign: TextAlign.center),
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // صورة الخلفية مع تأثير Blur
+              AnimatedBuilder(
+                animation: PageController(),
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: 1.0,
+                    child: Image.asset(
+                      'assets/images/welcome.png',
+                      fit: BoxFit.cover,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.height * 0.4,
+                      filterQuality: FilterQuality.high,
+                    ),
+                  );
+                },
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome to the working clinic application. How can we serve you?',
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+                maxLines: 3,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Show a dialog or navigate to the details screen
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text(
+                          'Congratulations',
+                          textAlign: TextAlign.center,
                         ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text('Go to Details'),
-            ),
-          ],
+                        content: Text(
+                          'You have successfully completed the onboarding process.',
+                          textAlign: TextAlign.center,
+                        ),
+                        titlePadding: EdgeInsets.all(20),
+                        contentTextStyle: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                        titleTextStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        backgroundColor: Colors.white,
+                        contentPadding: EdgeInsets.all(20),
+                        actionsPadding: EdgeInsets.all(10),
+                        actionsAlignment: MainAxisAlignment.center,
+                        actionsOverflowButtonSpacing: 10,
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Close', textAlign: TextAlign.center),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Go to Details'),
+              ),
+            ],
+          ),
         ),
       ),
     );
